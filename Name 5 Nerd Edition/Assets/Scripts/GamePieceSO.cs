68b28c3c65bc191d95fe7b1dfe941a0351093ff3
Shelldin <sheldon.bean@gmail.com>
2022-12-25 16:23:51 -0700

    Project Creation

---
+++ Name 5 Nerd Edition/Assets/Scripts/GamePieceSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "GamePieceSO")]
+
+public class GamePieceSO : ScriptableObject
+{
+   public SpriteRenderer pieceSprite;
+
+   public bool activePiece = false;
+   
+   public Vector2 currentPos;
+   public Vector2 nextPos;
+}
