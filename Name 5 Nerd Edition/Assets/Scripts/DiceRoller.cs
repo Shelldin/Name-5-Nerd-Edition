aa7a3248c70e60cc45e9bfcc56b11392b17ed1a7
Shelldin <sheldon.bean@gmail.com>
2022-12-28 18:23:12 -0700

    DiceRoll and result anims play on when dice roll button is pressed

--- Name 5 Nerd Edition/Assets/Scripts/DiceRoller.cs
+++ Name 5 Nerd Edition/Assets/Scripts/DiceRoller.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class DiceRoller : MonoBehaviour
 {
-   public float rollAnimationTime;
-   
+   public float rollAnimationTime = 3f;
+   private int rollResultInt;
+
+   private int minRoll = 1,
+      maxRoll = 6;
+
+   public Animator anim;
+
    private IEnumerator DiceRollCoroutine()
    {
+      //reset roll result so rolling animation functions properly
+      rollResultInt = 0;
+      anim.SetInteger("RollResultInt", rollResultInt);
+      
+      //dice rolling animation
+      anim.SetTrigger("DiceRollButtonPress");
+      
       yield return new WaitForSeconds(rollAnimationTime);
+
+      //randomly select which number the dice "lands" on and reflect that in the animation.
+      rollResultInt = Random.Range(minRoll, maxRoll);
+      anim.SetInteger("RollResultInt", rollResultInt);
    }
 
    public void RollDiceButton()
