74da25ec7e77be785e9255756a6457bf11efade0
Shelldin <sheldon.bean@gmail.com>
2023-03-29 19:44:06 -0600

    more double category picking stuff

--- Name 5 Nerd Edition/Assets/Scripts/DoubleDownCategorySelectionEvent.cs
+++ Name 5 Nerd Edition/Assets/Scripts/DoubleDownCategorySelectionEvent.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DoubleDownCategorySelectionEvent : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
     public CategoryChoiceCounterSO categoryChoiceCountSO;
+
+    public Image categoryImage;
+    public TMP_Text categoryText;
     
     public bool categoryHasBeenSelected = false;
 
@@ -20,23 +25,36 @@ public class DoubleDownCategorySelectionEvent : MonoBehaviour, IPointerUpHandler
         if (isDoubleDownTurn)
         {
             if (GameManager.instance.currentPlayerPieceSOList[GameManager.instance.currentPlayerTurnCount].currentSpace.layer
-                == LayerMask.NameToLayer("DoubleDownSpace") && !categoryHasBeenSelected)
+                == LayerMask.NameToLayer("DoubleDownSpace") && !categoryHasBeenSelected && categoryChoiceCountSO.categoriesChosenInt < 1)
             {
-
-                for (int i = 0; i < categoryGameObjList.Count; i++)
-                {
-                    if (!categoryHasBeenSelected)
-                    {
-                        categoryGameObjList[i].gameObject.SetActive(false);
-                    }
-                }
-            
                 categoryHasBeenSelected = true;
-            
+                
+                //CODE TO CHANGE ALPHA VALUE OF IMAGE AND TEXT
+
                 //UIController.instance.WildCategoryChosen();
 
             }
         }
+
+        else if (GameManager.instance.currentPlayerPieceSOList[GameManager.instance.currentPlayerTurnCount].currentSpace.layer
+                 == LayerMask.NameToLayer("DoubleDownSpace") && !categoryHasBeenSelected && categoryChoiceCountSO.categoriesChosenInt == 1)
+        {
+            for (int i = 0; i < categoryGameObjList.Count; i++)
+            {
+                if (!categoryHasBeenSelected)
+                {
+                    categoryGameObjList[i].gameObject.SetActive(false);
+                }
+            }
+            
+            categoryHasBeenSelected = true;
+        }
+
+        /*else if (GameManager.instance.currentPlayerPieceSOList[GameManager.instance.currentPlayerTurnCount].currentSpace.layer
+                   == LayerMask.NameToLayer("DoubleDownSpace") && !categoryHasBeenSelected && categoryChoiceCountSO.categoriesChosenInt > 1)
+        {
+            
+        }*/
     }
 
     public void OnPointerDown(PointerEventData eventData)
